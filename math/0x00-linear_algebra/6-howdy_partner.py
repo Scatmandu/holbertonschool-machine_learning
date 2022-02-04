24e9174bf72ab743219f165ae66576c3c17487c0
Scatmandu <mcclur05@nsuok.edu>
2022-02-04 14:28:37 -0600

    docstrings

--- math/0x00-linear_algebra/6-howdy_partner.py
+++ math/0x00-linear_algebra/6-howdy_partner.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
+"""concatenates two arrays"""
 
 
 def cat_arrays(arr1, arr2):
+    """concatenates two arrays"""
     new_list = []
     new_list = arr1 + arr2
     return new_list
