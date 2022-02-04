04d53d6c59f64f8d6a3e53360195fad05dd327cb
Scatmandu <mcclur05@nsuok.edu>
2022-02-04 14:18:30 -0600

    added file 12

---
+++ math/0x00-linear_algebra/12-bracin_the_elements.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""adds, subtracts, divides, multiplies two matrices"""
+
+
+def np_elementwise(mat1, mat2):
+    """adds, subtracts, divides, multiplies two matrices"""
+    return mat1 + mat2, mat1 - mat2, mat1 * mat2, mat1 / mat2
