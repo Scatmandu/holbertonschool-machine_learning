b9a65e8c3d70696564c8da921011aa9bbc33c928
Scatmandu <mcclur05@nsuok.edu>
2022-02-04 14:38:50 -0600

    lol I had the wrong code in here

--- math/0x00-linear_algebra/8-ridin_bareback.py
+++ math/0x00-linear_algebra/8-ridin_bareback.py
@@ -1,10 +1,19 @@
 #!/usr/bin/env python3
-
-import numpy as np
+"""multiplies two matrices"""
 
 
 def mat_mul(mat1, mat2):
+    """multiplies two matrices"""
 
-    product = np.dot(mat1, mat2)
-    x = product.tolist()
-    return x
+    if len(mat1) != len(mat2):
+        return None
+    elif len(mat1[0]) != len(mat2[0]):
+        return None
+    else:
+        result = []
+        for a, b in zip(mat1, mat2):
+            current_list = []
+            for x, y in zip(a, b):
+                current_list.append(x * y)
+            result.append(current_list)
+        return result
