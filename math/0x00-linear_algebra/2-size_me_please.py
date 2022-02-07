5bd5a9072aaf6580807df0e1b802fb3aa0c6d229
Scatmandu <mcclur05@nsuok.edu>
2022-02-07 17:14:48 -0600

    accounted for 1d matrix

--- math/0x00-linear_algebra/2-size_me_please.py
+++ math/0x00-linear_algebra/2-size_me_please.py
@@ -5,13 +5,21 @@
 def matrix_shape(matrix):
     """calculates the shape of two matrices"""
     new_list = []
-    row = len(matrix)
-    column = len(matrix[0])
-    if type(matrix[0][0]) is int:
+
+    if type(matrix[0]) is int:
+        row = len(matrix)
+        new_list.append(row)
+        return(new_list)
+
+    elif type(matrix[0][0]) is int:
+        row = len(matrix)
+        column = len(matrix[0])
         new_list.append(row)
         new_list.append(column)
         return new_list
     else:
+        row = len(matrix)
+        column = len(matrix[0])
         length = len(matrix[0][0])
         new_list.append(row)
         new_list.append(column)
