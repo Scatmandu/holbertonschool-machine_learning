dc71f696bf1f73b6f02580ed2207b7e7774a09c0
Scatmandu <mcclur05@nsuok.edu>
2022-02-08 17:22:18 -0600

    added all files

---
+++ math/0x01-plotting/4-frequency.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+import numpy as np
+import matplotlib.pyplot as plt
+
+np.random.seed(5)
+student_grades = np.random.normal(68, 15, 50)
+bins/ticks = [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+plt.hist(student_grades, bins=bins/ticks, edgecolor="black")
+plt.xlabel("Grades")
+plt.ylabel("Number of Students")
+plt.title("Project A")
+plt.axis([0, 100, 0, 30])
+plt.xticks(ticks=bins/ticks)
+plt.show()
