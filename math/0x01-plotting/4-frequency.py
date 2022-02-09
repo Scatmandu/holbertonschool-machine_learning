58ff0d289e91e8731eab096af6fb6d70173cbb52
Scatmandu <mcclur05@nsuok.edu>
2022-02-09 02:13:45 -0600

    forgot to commit earlier xD

--- math/0x01-plotting/4-frequency.py
+++ math/0x01-plotting/4-frequency.py
@@ -4,11 +4,11 @@ import matplotlib.pyplot as plt
 
 np.random.seed(5)
 student_grades = np.random.normal(68, 15, 50)
-bins/ticks = [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
-plt.hist(student_grades, bins=bins/ticks, edgecolor="black")
+bins_ticks = [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+plt.hist(student_grades, bins=bins_ticks, edgecolor="black")
 plt.xlabel("Grades")
 plt.ylabel("Number of Students")
 plt.title("Project A")
 plt.axis([0, 100, 0, 30])
-plt.xticks(ticks=bins/ticks)
+plt.xticks(ticks=bins_ticks)
 plt.show()
