da073cbb5dc2f49fccad56365a57992e2b69272e
Scatmandu <mcclur05@nsuok.edu>
2022-03-07 14:14:31 -0600

    added 11-13

---
+++ supervised_learning/0x01-classification/11-neural_network.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python3
+"""creating the class Neural Network"""
+
+
+import numpy as np
+
+
+class NeuralNetwork:
+    """initializing Neural Network"""
+    def __init__(self, nx, nodes):
+        if type(nx) is not int:
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        if type(nodes) is not int:
+            raise TypeError("nodes must be an integer")
+        if nodes < 1:
+            raise ValueError("nodes must be a positive integer")
+        self.nx = nx
+        self.nodes = nodes
+        self.__W1 = np.random.normal(size=(nodes, nx))
+        self.__b1 = np.zeros((nodes, 1))
+        self.__A1 = 0
+        self.__W2 = np.random.normal(size=(1, nodes))
+        self.__b2 = 0
+        self.__A2 = 0
+
+    def forward_prop(self, X):
+        """calculates forward propagation"""
+        x = np.dot(self.W1, X) + self.__b1
+        self.__A1 = 1/(1 + np.exp(-x))
+        x = np.dot(self.W2, self.__A1) + self.__b2
+        self.__A2 = 1/(1 + np.exp(-x))
+        return self.__A1, self.__A2
+
+    def cost(self, Y, A):
+        """Calculates cost using logistic regression"""
+        m = Y.shape[1]
+        loss = np.sum(Y * np.log(A) + (1 - Y) * np.log(1.0000001 - A))
+        cost = -(1 / m) * (loss)
+        return (cost)
+
+    @property
+    def W1(self):
+        """W1 setter"""
+        return self.__W1
+
+    @property
+    def b1(self):
+        """b1 setter"""
+        return self.__b1
+
+    @property
+    def A1(self):
+        """A1 setter"""
+        return self.__A1
+
+    @property
+    def W2(self):
+        """W2 setter"""
+        return self.__W2
+
+    @property
+    def b2(self):
+        """b2 setter"""
+        return self.__b2
+
+    @property
+    def A2(self):
+        """A2 setter"""
+        return self.__A2
