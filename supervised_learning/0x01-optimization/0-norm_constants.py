d4e1a68215b652fcac4a5301cc2f0a80ef1c79b0
Scatmandu <mcclur05@nsuok.edu>
2022-03-21 23:53:52 -0500

    removed extra line

---
+++ supervised_learning/0x01-optimization/0-norm_constants.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""optimization"""
+import numpy as np
+
+
+def normalization_constants(X):
+    """Calculates the normalization constants of a matrix"""
+    return np.mean(X, axis=0), np.std(X, axis=0)
