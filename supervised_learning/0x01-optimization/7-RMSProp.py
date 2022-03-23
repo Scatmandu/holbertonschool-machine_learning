e259e521064e38ec9e0e0a71b4873405b8ad7b50
Scatmandu <mcclur05@nsuok.edu>
2022-03-22 22:27:50 -0500

    fixed typo in return

--- supervised_learning/0x01-optimization/7-RMSProp.py
+++ supervised_learning/0x01-optimization/7-RMSProp.py
@@ -5,7 +5,13 @@
 def update_variables_RMSProp(alpha, beta2, epsilon, var, grad, s):
     """
         method that updates a variable using the RMSProp optimization algorithm
+        alpha = learning rate
+        beta2 = RMSProp weight
+        epsilon = small number to avoid division by zero
+        var = numpy.ndarray containing the variable to be updated
+        grad = numpy.ndarray containing the gradient of var
+        s = the previous second moment of var
     """
-    moment = (beta2 * s) + ((1 - beta2) * (grad ** 2))
+    moment = beta2 * s + (1 - beta2) * (grad ** 2)
     update = var - (alpha * (grad / ((moment ** (0.5) + epsilon))))
-    return updated, moment
+    return update, moment
